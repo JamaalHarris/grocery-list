c0fe545197fed12bec7bf180c719edabb0120ace
JamaalHarris <jamaalmalikharris@gmail.com>
2023-04-10 16:27:23 -0500

    Add tests

--- test/controllers/groceries_controller_test.rb
+++ test/controllers/groceries_controller_test.rb
@@ -13,3 +13,34 @@ test "index" do
   data = JSON.parse(response.body)
   assert_equal Grocery.count, data.length
 end
+
+test "create" do
+  assert_difference "Grocery.count", 1 do
+    post "/groceries.json", params: { name: "banana", price: 1, quantity: 2, category: "fruit" }
+    assert_response 200
+  end
+end
+
+test "show" do
+  get "/groceriess/#{Grocery.first.id}.json"
+  assert_response 200
+
+  data = JSON.parse(response.body)
+  assert_equal ["name", "price", "quantity", "category"], data.keys
+end
+
+test "update" do
+  grocery = Grocery.first
+  patch "/groceries/#{grocery.id}.json", params: { name: "Updated name" }
+  assert_response 200
+
+  data = JSON.parse(response.body)
+  assert_equal "Updated name", data["name"]
+end
+
+test "destroy" do
+  assert_difference "Grocery.count", -1 do
+    delete "/groceries/#{Grocery.first.id}.json"
+    assert_response 200
+  end
+end
